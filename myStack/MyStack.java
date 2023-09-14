e2d620839a26a4b5a2519788e27fa151c6bb1045
Shrey <38576589+shreySodhi@users.noreply.github.com>
2023-09-13 23:20:54 -0500

    Update MyStack.java

--- myStack/MyStack.java
+++ myStack/MyStack.java
@@ -8,13 +8,15 @@ class MyStack {
     public MyStack() {
         this(7);
     }
-    public void clear(){
-        arr = new Integer[initSize];
-    }
+    
     MyStack(int initSize){
         this.initSize = initSize;
         arr = new Integer[initSize];
     }
+        public void clear(){
+        arr = new Integer[initSize];
+    }
+    
     public boolean isEmpty(){
         return size == 0;
     }
@@ -25,24 +27,29 @@ class MyStack {
         arr[size] = val;
         size++;
     }
+    
     public void doubleCapacity(){
         Integer [] arr2 = new Integer[arr.length*2];
         for(int x = 0; x < arr.length;x++)
             arr2[x] = arr[x];
         arr = arr2;
     }
+    
     public Integer pop() {
         if(size == 0)
             throw new EmptyStackException(); 
         size--;
         return arr[size];
     }
+    
     public int size(){
         return size;
     }
+    
     public Integer peek() {
         return  arr[size-1];
     }
+    
     @Override
     public String toString() {
         String s = "top -> ";
